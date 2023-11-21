cccbe731d88c712d8e2758bf33b4cb0c94a5c8a3
emperatrizm <emperatriz.668@gmail.com>
2023-11-20 19:49:44 -0600

    finishing home

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,51 +4,48 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
-console.log({
+{/*console.log({
     clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-})
+})*/}
 const handler = NextAuth({
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      })
     ],
-    async session({ session }){
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
+ 
+      async session({ session }) {
+        // store the user id from MongoDB to session
+        const sessionUser = await User.findOne({ email: session.user.email });
         session.user.id = sessionUser._id.toString();
-
+  
         return session;
-        
-    },
-    async signIn({ profile }){
+      },
+      async signIn({ account, profile, user, credentials }) {
         try {
-            await connectToDB();
-
-            //revisa si existe usuario
-                const userExists = await User.findOne({
-                    email: profile.email
-                });
-            //crear uno nuevo si no existe
-            if(!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", " ").toLoweCase(),
-                    image: profile.picture
-                })
-            }
-
-            return true;
+          await connectToDB();
+  
+          // check if user already exists
+          const userExists = await User.findOne({ email: profile.email });
+  
+          // if not, create a new document and save user in MongoDB
+          if (!userExists) {
+            await User.create({
+              email: profile.email,
+              username: profile.name.replace(" ", "").toLowerCase(),
+              image: profile.picture,
+            });
+          }
+  
+          return true
         } catch (error) {
-            console.log(error);
-            return false;
-
+          console.log("Error checking if user exists: ", error.message);
+          return false
         }
-    },
+      },
+    
 })
 
 export {handler as GET, handler as POST};
